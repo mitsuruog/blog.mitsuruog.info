3e6516121d9380035e218a4a909e340d4e6ae543
Mitsuru Ogawa <mitsuru.ogawa.jp@gmail.com>
2016-01-18 10:02:51 +0900

    静的ファイル作成タスクを追加した

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var textlint = require('gulp-textlint');
+var del = require('del');
 
 gulp.task('textlint', function() {
   return gulp.src('./source/**/*.md')
@@ -8,6 +9,10 @@ gulp.task('textlint', function() {
 
 gulp.task('watch', function() {
   gulp.watch('./source/**/*.md', ['textlint']);
+})
+
+gulp.task('clean', function() {
+  del(['public/**']);
 })
 
 gulp.task('default', ['textlint', 'watch']);
