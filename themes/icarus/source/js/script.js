a19ad6b51bb516e944a8fdf3b85c190a348f61e7
Mitsuru Ogawa <mitsuru.ogawa.jp@gmail.com>
2017-11-28 11:51:35 +0900

    Remove SNS share features

--- themes/icarus/source/js/script.js
+++ themes/icarus/source/js/script.js
@@ -1,60 +1,60 @@
 (function($){
   var toTop = $('#toTop').length ? $('#toTop').offset().top - $(window).height() + 20 : 0;
-  
+
   // Share
-  $('body').on('click', function(){
-    $('.article-share-box.on').removeClass('on');
-  }).on('click', '.article-share-link', function(e){
-    e.stopPropagation();
-
-    var $this = $(this),
-      url = $this.attr('data-url'),
-      encodedUrl = encodeURIComponent(url),
-      id = 'article-share-box-' + $this.attr('data-id'),
-      offset = $this.offset();
-
-    if ($('#' + id).length){
-      var box = $('#' + id);
-
-      if (box.hasClass('on')){
-        box.removeClass('on');
-        return;
-      }
-    } else {
-      var html = [
-        '<div id="' + id + '" class="article-share-box">',
-          '<input class="article-share-input" value="' + url + '">',
-          '<div class="article-share-links">',
-            '<a href="https://twitter.com/intent/tweet?url=' + encodedUrl + '" class="fa fa-twitter article-share-twitter" target="_blank" title="Twitter"></a>',
-            '<a href="https://www.facebook.com/sharer.php?u=' + encodedUrl + '" class="fa fa-facebook article-share-facebook" target="_blank" title="Facebook"></a>',
-            // '<a href="http://pinterest.com/pin/create/button/?url=' + encodedUrl + '" class="fa fa-pinterest article-share-pinterest" target="_blank" title="Pinterest"></a>',
-            '<a href="http://b.hatena.ne.jp/entry/' + encodedUrl + '" class="hatena-bookmark-button" data-hatena-bookmark-layout="simple-balloon" title="このエントリーをはてなブックマークに追加"><img src="https://b.st-hatena.com/images/entry-button/button-only@2x.png" alt="このエントリーをはてなブックマークに追加" width="20" height="20" style="border: none;" /></a>',
-            // '<a href="https://plus.google.com/share?url=' + encodedUrl + '" class="fa fa-google article-share-google" target="_blank" title="Google+"></a>',
-          '</div>',
-        '</div>'
-      ].join('');
-
-      var box = $(html);
-
-      $('body').append(box);
-    }
-
-    $('.article-share-box.on').hide();
-
-    box.css({
-      top: offset.top + 25,
-      left: offset.left
-    }).addClass('on');
-  }).on('click', '.article-share-box', function(e){
-    e.stopPropagation();
-  }).on('click', '.article-share-box-input', function(){
-    $(this).select();
-  }).on('click', '.article-share-box-link', function(e){
-    e.preventDefault();
-    e.stopPropagation();
-
-    window.open(this.href, 'article-share-box-window-' + Date.now(), 'width=500,height=450');
-  });
+  // $('body').on('click', function(){
+  //   $('.article-share-box.on').removeClass('on');
+  // }).on('click', '.article-share-link', function(e){
+  //   e.stopPropagation();
+  //
+  //   var $this = $(this),
+  //     url = $this.attr('data-url'),
+  //     encodedUrl = encodeURIComponent(url),
+  //     id = 'article-share-box-' + $this.attr('data-id'),
+  //     offset = $this.offset();
+  //
+  //   if ($('#' + id).length){
+  //     var box = $('#' + id);
+  //
+  //     if (box.hasClass('on')){
+  //       box.removeClass('on');
+  //       return;
+  //     }
+  //   } else {
+  //     var html = [
+  //       '<div id="' + id + '" class="article-share-box">',
+  //         '<input class="article-share-input" value="' + url + '">',
+  //         '<div class="article-share-links">',
+  //           '<a href="https://twitter.com/intent/tweet?url=' + encodedUrl + '" class="fa fa-twitter article-share-twitter" target="_blank" title="Twitter"></a>',
+  //           '<a href="https://www.facebook.com/sharer.php?u=' + encodedUrl + '" class="fa fa-facebook article-share-facebook" target="_blank" title="Facebook"></a>',
+  //           // '<a href="http://pinterest.com/pin/create/button/?url=' + encodedUrl + '" class="fa fa-pinterest article-share-pinterest" target="_blank" title="Pinterest"></a>',
+  //           '<a href="http://b.hatena.ne.jp/entry/' + encodedUrl + '" class="hatena-bookmark-button" data-hatena-bookmark-layout="simple-balloon" title="このエントリーをはてなブックマークに追加"><img src="https://b.st-hatena.com/images/entry-button/button-only@2x.png" alt="このエントリーをはてなブックマークに追加" width="20" height="20" style="border: none;" /></a>',
+  //           // '<a href="https://plus.google.com/share?url=' + encodedUrl + '" class="fa fa-google article-share-google" target="_blank" title="Google+"></a>',
+  //         '</div>',
+  //       '</div>'
+  //     ].join('');
+  //
+  //     var box = $(html);
+  //
+  //     $('body').append(box);
+  //   }
+  //
+  //   $('.article-share-box.on').hide();
+  //
+  //   box.css({
+  //     top: offset.top + 25,
+  //     left: offset.left
+  //   }).addClass('on');
+  // }).on('click', '.article-share-box', function(e){
+  //   e.stopPropagation();
+  // }).on('click', '.article-share-box-input', function(){
+  //   $(this).select();
+  // }).on('click', '.article-share-box-link', function(e){
+  //   e.preventDefault();
+  //   e.stopPropagation();
+  //
+  //   window.open(this.href, 'article-share-box-window-' + Date.now(), 'width=500,height=450');
+  // });
 
   // Caption
   $('.article-entry').each(function(i){
